d1280d5f199a2a6d9bed1f00b2784f2acd1ade82
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-04-05 19:19:57 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,22 @@
+var number = 5
+  function add(a, b){
+    return number + number;
+  }
+  
+   function subtract(a, b) {
+    number = a - b;
+    return number;
+  }
+  function multiply(a, b) {
+    number = a * b 
+    return number
+  }
+  function divide(a, b) {
+    number = a / b
+    return number
+  }
+  function inc(n) {
+    number+=n
+    return number
+  }
+  
